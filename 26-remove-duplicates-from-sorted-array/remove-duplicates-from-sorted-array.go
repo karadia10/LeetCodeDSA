622502b2c280a7b2c2c49eee790c091651c34167
Vikas Raj Karadia <karadia10@gmail.com>
2025-01-11 19:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (50.49%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -1,10 +1,10 @@
 func removeDuplicates(nums []int) int {
-    w:=0
+    w:=1
     for r:=0;r<len(nums);r++ {
-        if nums[w]<nums[r] {
-            nums[w+1] = nums[r]
+        if nums[w-1]<nums[r] {
+            nums[w] = nums[r]
             w++
         }
     }
-    return w+1
+    return w
 }
