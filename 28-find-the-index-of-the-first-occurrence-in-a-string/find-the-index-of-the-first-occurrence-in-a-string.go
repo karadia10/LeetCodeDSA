4fb5cf24ad2b92e7674f3d69d29a6f90680a2ea6
Vikas Raj Karadia <karadia10@gmail.com>
2025-01-12 08:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (34.52%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -3,19 +3,24 @@ func strStr(haystack string, needle string) int {
         return -1
     }
     for i:=0;i<=(len(haystack)-len(needle));i++ {
-        if (haystack[i]==needle[0]) {
-            found:=true
-            for j:=0;j<len(needle);j++ {
-                if haystack[i+j]!=needle[j] {
-                    found=false
-                    break
-                } else {
-                    found = true
-                }
-            }
-            if found {
-                return i
-            }
+        //Approach 1: O(h*n)
+        // if (haystack[i]==needle[0]) {
+        //     found:=true
+        //     for j:=0;j<len(needle);j++ {
+        //         if haystack[i+j]!=needle[j] {
+        //             found=false
+        //             break
+        //         } else {
+        //             found = true
+        //         }
+        //     }
+        //     if found {
+        //         return i
+        //     }
+        // }
+        //Approach 2: O(h*n)
+        if haystack[i:len(needle)+i] == needle {
+            return i
         }
     }
     return -1
