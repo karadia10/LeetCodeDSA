1b168700a32997920d05d21f1e7f8a62c5d4dca9
Vikas Raj Karadia <karadia10@gmail.com>
2025-05-11 14:53:40 +0530

    Time: 1 ms (31.73%) | Memory: 6.3 MB (56.83%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.go
+++ 2-add-two-numbers/add-two-numbers.go
@@ -13,21 +13,15 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
         s:= carry
         if l1!=nil {
             s+=l1.Val
+            l1 = l1.Next
         }
         if l2!=nil {
             s+=l2.Val
+            l2 = l2.Next
         }
         carry = s/10
         ptr.Next = &ListNode{Val: s % 10}
         ptr = ptr.Next 
-        if l1!= nil { 
-            fmt.Println("l1")
-            l1 = l1.Next
-        }
-        if l2 != nil {
-            fmt.Println("l2")
-            l2 = l2.Next
-        }
     }
     return root.Next
 }
