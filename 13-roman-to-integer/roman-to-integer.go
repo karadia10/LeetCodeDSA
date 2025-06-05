b7caff4ec35fd061e4b4f9b9fd3f9a5cca3cf42a
Vikas Raj Karadia <karadia10@gmail.com>
2025-06-06 00:07:19 +0530

    Time: 2 ms (50.02%) | Memory: 4.8 MB (57.71%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.go
@@ -0,0 +1,21 @@
+func romanToInt(s string) int {
+    m:=make(map[byte]int, 0)
+    m['I'] = 1
+    m['V'] = 5
+    m['X'] = 10
+    m['L'] = 50
+    m['C'] = 100
+    m['D'] = 500
+    m['M'] = 1000
+    sum:=m[s[len(s)-1]]
+    // fmt.Println("",s[2])
+    for i:= len(s) - 2; i >= 0; i-- {
+        // fmt.Println(s[i])
+        if m[s[i]] < m[s[i+1]] {
+            sum-=m[s[i]]
+        } else {
+            sum+=m[s[i]]
+        }
+    }
+    return sum
+}
