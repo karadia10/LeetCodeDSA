5046a97807cc60691267c80d82a0f10ec5000977
Vikas Raj Karadia <karadia10@gmail.com>
2025-06-06 00:09:40 +0530

    Time: 1 ms (62.48%) | Memory: 4.8 MB (57.71%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.go
+++ 13-roman-to-integer/roman-to-integer.go
@@ -1,16 +1,15 @@
 func romanToInt(s string) int {
-    m:=make(map[byte]int, 0)
-    m['I'] = 1
-    m['V'] = 5
-    m['X'] = 10
-    m['L'] = 50
-    m['C'] = 100
-    m['D'] = 500
-    m['M'] = 1000
+    // m:=make(map[byte]int, 0)
+    m:=map[byte]int{'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    // m['I'] = 1
+    // m['V'] = 5
+    // m['X'] = 10
+    // m['L'] = 50
+    // m['C'] = 100
+    // m['D'] = 500
+    // m['M'] = 1000
     sum:=m[s[len(s)-1]]
-    // fmt.Println("",s[2])
     for i:= len(s) - 2; i >= 0; i-- {
-        // fmt.Println(s[i])
         if m[s[i]] < m[s[i+1]] {
             sum-=m[s[i]]
         } else {
