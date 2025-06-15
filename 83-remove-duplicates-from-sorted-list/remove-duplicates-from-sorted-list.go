48696514f0e2416131c3911f04bfadd2edf11e4a
Vikas Raj Karadia <karadia10@gmail.com>
2025-06-16 01:00:13 +0530

    Time: 0 ms (100.00%) | Memory: 4.9 MB (24.85%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -7,20 +7,12 @@
  */
 func deleteDuplicates(head *ListNode) *ListNode {
     curr:=head
-    for curr != nil {
-        if curr.Next!=nil && curr.Next.Val == curr.Val {
-            // fmt.Println("1")
-            if curr.Next.Next != nil {
-                // fmt.Println("2", curr.Next.Next)
-                curr.Next = curr.Next.Next
-            } else {
-                // fmt.Println("3") 
-                curr.Next=nil
-            }
-            continue
+    for curr != nil && curr.Next!=nil {
+        if  curr.Next.Val == curr.Val {
+            curr.Next = curr.Next.Next
+        } else {
+            curr=curr.Next
         }
-        fmt.Println(curr)
-        curr=curr.Next
     }
     return head
 }
