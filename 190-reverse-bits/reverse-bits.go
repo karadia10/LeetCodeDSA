9e96d28d5dfad33e0411fd06f24e07471c566f81
Vikas Raj Karadia <karadia10@gmail.com>
2025-06-16 00:24:56 +0530

    Time: 0 ms (100.00%) | Memory: 4.3 MB (52.17%) - LeetSync

--- 190-reverse-bits/reverse-bits.go
+++ 190-reverse-bits/reverse-bits.go
@@ -1,9 +1,8 @@
 func reverseBits(num uint32) uint32 {
     var res uint32 = 0
     for i:=0;i<32;i++ {
-        x:=num&1
         res=res<<1
-        res+=x
+        res+=num&1
         num=num>>1
     }
     return res
