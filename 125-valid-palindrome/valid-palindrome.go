7d179c32265847bdf9039da586867edf67dd191d
Vikas Raj Karadia <karadia10@gmail.com>
2025-06-19 22:50:44 +0530

    Time: 103 ms (10.72%) | Memory: 5.9 MB (30.94%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.go
@@ -0,0 +1,24 @@
+func isPalindrome(s string) bool {
+    l:=0
+    r:=len(s)-1
+    for l<=r {
+        fmt.Println(l, r)
+        if !isAlphabet(s[l]){
+            l++
+            continue
+        }
+        if !isAlphabet(s[r]) {
+            r--
+            continue
+        }
+        if (unicode.ToUpper(rune(s[l])) != unicode.ToUpper(rune(s[r]))) {
+            return false
+        } 
+        l++
+        r--
+    }
+    return true
+}
+func isAlphabet(a byte) bool {
+    return ((a>='a' && a<='z') || (a>='A' && a<='Z')) || (a>='0' && a<='9')
+}
